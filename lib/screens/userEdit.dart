758b8e1c397fa1171d032f4173eab156b334fdf8
Akworld18 <arun@deepsense.in>
2023-04-20 20:35:46 +0530

    new app created

---
+++ lib/screens/userEdit.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class UserEdit extends StatefulWidget {
+  const UserEdit({Key? key}) : super(key: key);
+
+  @override
+  State<UserEdit> createState() => _UserEditState();
+}
+
+class _UserEditState extends State<UserEdit> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(child:Column(
+        children: [
+
+        ],
+      )),
+    );
+  }
+}
